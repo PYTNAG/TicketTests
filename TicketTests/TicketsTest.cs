16bfc6e0dd7494d508136e527f7c4ccb3150a774
PYTNAG <sgds.2020.nag@yandex.ru>
2024-05-11 00:20:57 +0500

    Add tickets test

---
+++ TicketTests/TicketsTest.cs
@@ -0,0 +1,55 @@
+using System.Text.RegularExpressions;
+
+namespace TicketTests;
+
+[TestFixture]
+public class TicketsTest
+{
+    [Test, TestCaseSource(typeof(TicketsSource), nameof(TicketsSource.TicketFiles))]
+    public void TestTicketsSource(string ticketFile)
+    {
+        Assert.DoesNotThrow(() =>
+        {
+            Ticket ticket = TicketParser.Parse(ticketFile);
+            
+            Assert.DoesNotThrow(() => { var _ = ticket.DepartureDate; }, "Departure date has wrong format.");
+            
+            AssertCorrectStationName(ticket.From);
+            AssertCorrectStationName(ticket.To);
+            
+            Assert.That(ticket.SystemNumber, Does.EndWith(ticket.Number), "System number must end with ticket number.");
+            Assert.That(ticket.SystemNumber[3], Is.EqualTo('1'), "System number must have fourth digit equals to 1.");
+
+            Assert.That(ticket.LongTerm, Is.AnyOf("Разовый", "Многоразовый"), "Wrong ticket's long term.");
+            Assert.That(ticket.Fullness, Is.AnyOf("Полный", "Частичный"), "Wrong ticket's fullness.");
+
+            Assert.That(ticket.FareCost,
+                ticket.Fullness == "Полный" 
+                    ? Is.EqualTo(ticket.BaseCost) 
+                    : Is.LessThan(ticket.BaseCost));
+            
+            Assert.That(ticket.TotalCost / ticket.FareCost,
+                ticket.LongTerm == "Разовый"
+                ? Is.EqualTo(1.0f)
+                : Is.GreaterThan(1.0f));
+        });
+    }
+
+    private void AssertCorrectStationName(string stationName)
+    {
+        Assert.That(
+            stationName, Does.Not.Match(@"\s\.|\.\S"), 
+            "Dot must be placed after word and have a space after. Example: \"Sentence. Sentence\"");
+        Assert.That(
+            stationName, Does.Not.Match(@"[\D-[\s]]\d+|\d+[\D-[\s]]"),
+            "Number must be isolated with spaces. Example: \"Sentence. 74 Sentence\"");
+        Assert.That(
+            stationName, Does.Not.Match(@"[\W\d]-{1}|-{1}[\W\d]"),
+            "Hyphen must be sandwiched between letters. Example: \"Word-Word\"");
+    }
+
+    private class TicketsSource
+    {
+        public static IEnumerable<string> TicketFiles => Directory.GetFiles("promit", "*.txt");
+    }
+}
